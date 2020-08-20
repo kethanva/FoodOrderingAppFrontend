e86436bce1fc3897b34902e3124f020de4ee76fe
priteeshh <priteeshh@gmail.com>
2020-08-21 04:07:12 +0530

    Designed Home page and completed service integration

---
+++ src/screens/card/CardCompnent.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import './CardCompnent.css';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import Star from '@material-ui/icons/Star';
+
+
+class CardCompnent extends Component {
+    constructor() {
+        super();
+        this.state = {
+            restaurant: {}
+
+        };
+    }
+    componentDidMount() {
+        this.setState({
+            restaurant: this.props.restaurant
+        });
+    }
+    render() {
+        return (
+            <div>
+                <Card className="root" onClick={this.props.clickDetailHandler}>
+                    <img className="restaurant-image" src={this.state.restaurant.photo_URL} alt="Profile Pic" /><br /><br />
+                    <CardContent className="card-container">
+                        <div className="name">
+                            <Typography gutterBottom variant="h5" component="h2">
+                                {this.state.restaurant.restaurant_name}
+                            </Typography>
+                        </div>
+                        <div className="category">
+                            <Typography variant="body2" color="textSecondary" component="p">
+                                {this.state.restaurant.categories}
+                            </Typography>
+                        </div>
+                        <div className="bottom">
+                            <div className="ratings">
+                                <Star className="star-icon"></Star>
+                                <label className="ratingLabel"><p>{Math.round((this.state.restaurant.customer_rating + Number.EPSILON) * 100) / 100} ({this.state.restaurant.number_customers_rated})</p></label>
+                            </div>
+                            <div className="price-Container">
+                                <label className="price"><p>&#x20B9; {this.state.restaurant.average_price} for two</p></label>
+                            </div>
+
+                        </div>
+                    </CardContent>
+
+                </Card>
+            </div>
+        )
+    }
+}
+export default CardCompnent;
--- src/screens/home/Home.js
+++ src/screens/home/Home.js
@@ -1,16 +1,56 @@
 import React, { Component } from "react";
 import './Home.css';
 import Header from '../../common/header/Header'
+import Card from '../card/CardCompnent';
+import Grid from '@material-ui/core/Grid';
 
-class Home extends Component{
+class Home extends Component {
+    constructor() {
+        super();
+        this.state = {
+            restaurants: []
+        }
+    }
     clickProfileHandler = () => {
         this.props.history.push("/profile");
     }
-    render(){
-        return(
+    clickDetailHandler = () => {
+        this.props.history.push("/detail");
+
+    }
+    componentDidMount() {
+        let thisComponent = this;
+        let xhrData = new XMLHttpRequest();
+        let restaurants = null;
+        xhrData.addEventListener("readystatechange", function () {
+            if (this.readyState === 4 && this.status === 200) {
+                thisComponent.setState({
+                    restaurants: JSON.parse(this.response).restaurants
+
+                });
+                console.log(JSON.parse(this.response).restaurants);
+
+            }
+        });
+        xhrData.open("GET", 'http://localhost:8080/api/restaurant');
+        xhrData.send(restaurants);
+    }
+    render() {
+
+        return (
             <div>
-            <Header page="home" clickProfile={this.clickProfileHandler}></Header>
-            Home
+                <Header page="home" clickProfile={this.clickProfileHandler}></Header>
+                <Grid container className="root">
+                    <Grid item xs={12}>
+                        <Grid container justify="center" spacing={1}>
+                            {this.state.restaurants.map((restaurant) => (
+                                <Grid key={restaurant.id} item>
+                                    <Card restaurant={restaurant} key={restaurant.id} clickDetailHandler={this.clickDetailHandler}></Card>
+                                </Grid>
+                            ))}
+                        </Grid>
+                    </Grid>
+                </Grid>
             </div>
         )
     }
